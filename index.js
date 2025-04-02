ee7c188e50f87dc456218e771f767ea43e37a797
StefanoRC <victorstefanorc@gmail.com>
2025-04-02 15:21:53 +0000

    feat: final exam resolution

---
+++ index.js
@@ -0,0 +1,10 @@
+function suamr (a,b){
+	let resultado = a+ b;
+}
+
+function mostrarSuma(x, y){
+	let resultadoSuma = sumar (x, y);
+	console.log("el resultado de la suma es: ", resultadoSuma)
+}
+
+console.log(mostrarSuma(10, 5))
